7f814131842a7a2d9f4ac1044dbcb905153c483f
Mohit <kumarmohit2496@gmail.com>
2021-06-12 21:52:09 +0530

    UI elements added for Team Page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import TeamPage from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage/>
+      <Router>
+        {/* Switch is required because url will match both the routes. So, both will be loaded. Switch loads the first match */}
+        <Switch> 
+
+          <Route path="/teams/:teamName/matches/:year">
+            <MatchPage />
+          </Route>
+
+          <Route path="/teams/:teamName">
+            <TeamPage />
+          </Route>
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,30 +1,35 @@
 import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router';
 import { MatchDetailCard } from '../components/MatchDetailCard'
 import { MatchSmallCard } from '../components/MatchSmallCard'
 
 export const TeamPage = () =>{
 
   const [team, setTeam] = useState({matches: []});
-
+  const {teamName} = useParams();
   useEffect(
         () => {
             const fetchMatches = async () => {
-                const response = await fetch('http://localhost:8080/teams/Delhi Capitals')
+                const response = await fetch(`http://localhost:8080/teams/${teamName}`)
                 const data = await response.json();
                 setTeam(data);
             };
             fetchMatches();
-        }, [] //Call the useEffect only when something changes in this array. Here array is empty, so, useEffect is not going to call itself again and again. 
-              //We are fetching data and putting into state. this is changing Team . Hence, useState is called again and again
-        
-    )
+        }, [teamName] //Call the useEffect only when something changes in this array. Here array is empty, so, useEffect is not going to call itself again and again. 
+              //We are fetching data and putting into state. this is changing Team . Hence, useState is called again and again      
+  );
+  
+  if(!team || !team.teamName){
+        return <h1>Team Not Found</h1>
+  }
+  
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]}/>
+      <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
       {/* Calling MatchSmallCard as many times as there are elements in the array 'team.matches' 
         Slice slices the array from1 to n as we are already displaying the latest match in Detail Card*/}
-      {team.matches.slice(1).map(match => <MatchSmallCard match={match}/>)}  
+      {team.matches.slice(1).map(match => <MatchSmallCard key={match.id} teamName={team.teamName} match={match}/>)}  
     </div>
   );
 }
